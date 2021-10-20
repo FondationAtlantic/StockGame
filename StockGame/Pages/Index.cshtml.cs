c5f1d56d5c04c7f83190ed29e80993e13abf6509
Andronik Karkaselis <akarkaselis@nmedia.ca>
2021-10-20 17:07:38 -0400

    Team joining

--- StockGame/Pages/Index.cshtml.cs
+++ StockGame/Pages/Index.cshtml.cs
@@ -24,41 +24,47 @@ namespace StockGame.Pages
         public async Task OnGetAsync()
         {
             await FindActiveGameAndTeam();
+            //HasJoinedTeam = Context.TeamMembers.Where(tm => tm.UserId == CurrentUser.Id).FirstOrDefault() != null;
+
             if (User.Identity.IsAuthenticated && User.IsInRole("Student"))
             {
                 HasJoinedTeam = Context.TeamMembers.Where(tm => tm.UserId == CurrentUser.Id).FirstOrDefault() != null;
-                await FetchPortfolio();
-
-                await FindActiveEpisodeEquityInfos();
-
-                var items = new List<AnalysisIndexItem>();
-
-                var iterPastEquityInfos = ActiveAndPastEpisodeEquityInfos.GetEnumerator();
-                iterPastEquityInfos.MoveNext();
-                foreach (EpisodeEquityInfo eei in ActiveEpisodeEquityInfos)
+                
+                if (HasJoinedTeam)
                 {
-                    while (iterPastEquityInfos.Current != null
-                            && iterPastEquityInfos.Current.EquityId == eei.EquityId
-                            && iterPastEquityInfos.Current.Episode.EpisodeIndex != ActiveEpisodeIndex - 1)
+                    await FetchPortfolio();
+                    await FindActiveEpisodeEquityInfos();
+
+                    var items = new List<AnalysisIndexItem>();
+
+                    var iterPastEquityInfos = ActiveAndPastEpisodeEquityInfos.GetEnumerator();
+                    iterPastEquityInfos.MoveNext();
+                    foreach (EpisodeEquityInfo eei in ActiveEpisodeEquityInfos)
                     {
+                        while (iterPastEquityInfos.Current != null
+                                && iterPastEquityInfos.Current.EquityId == eei.EquityId
+                                && iterPastEquityInfos.Current.Episode.EpisodeIndex != ActiveEpisodeIndex - 1)
+                        {
+                            iterPastEquityInfos.MoveNext();
+                        }
+
+                        if (eei.Visible)
+                        {
+                            items.Add(new AnalysisIndexItem
+                            {
+                                EpisodeEquityInfo = eei,
+                                Trend = (iterPastEquityInfos.Current == null || iterPastEquityInfos.Current.EquityId != eei.EquityId || iterPastEquityInfos.Current.Price == eei.Price)
+                                        ? AnalysisIndexItem.PriceTrend.Unchanged
+                                        : (iterPastEquityInfos.Current.Price < eei.Price ? AnalysisIndexItem.PriceTrend.Up : AnalysisIndexItem.PriceTrend.Down)
+                            });
+                        }
+
+                        iterPastEquityInfos.MoveNext();
                         iterPastEquityInfos.MoveNext();
                     }
-
-                    if (eei.Visible)
-                    {
-                        items.Add(new AnalysisIndexItem
-                        {
-                            EpisodeEquityInfo = eei,
-                            Trend = (iterPastEquityInfos.Current == null || iterPastEquityInfos.Current.EquityId != eei.EquityId || iterPastEquityInfos.Current.Price == eei.Price)
-                                    ? AnalysisIndexItem.PriceTrend.Unchanged
-                                    : (iterPastEquityInfos.Current.Price < eei.Price ? AnalysisIndexItem.PriceTrend.Up : AnalysisIndexItem.PriceTrend.Down)
-                        });
-                    }
-
-                    iterPastEquityInfos.MoveNext();
-                    iterPastEquityInfos.MoveNext();
+                    IndexItems = items.OrderBy(item => item.EpisodeEquityInfo.Equity.Industry.Name).ThenBy(item => item.EpisodeEquityInfo.Equity.Name).ToList();
                 }
-                IndexItems = items.OrderBy(item => item.EpisodeEquityInfo.Equity.Industry.Name).ThenBy(item => item.EpisodeEquityInfo.Equity.Name).ToList();
+                
             }
         }
 
@@ -66,11 +72,14 @@ namespace StockGame.Pages
         {
             await FindActiveGameAndTeam();
 
-            PortfolioGameHistory pgh = await PortfolioHistories(ActiveGame, Enumerable.Repeat(ActiveTeam, 1), ActiveEpisodeIndex);
+            if (User.Identity.IsAuthenticated && User.IsInRole("Student"))
+            {
+                PortfolioGameHistory pgh = await PortfolioHistories(ActiveGame, Enumerable.Repeat(ActiveTeam, 1), ActiveEpisodeIndex);
 
-            PortfolioTeamHistory pth = pgh.TeamHistories[0];
+                PortfolioTeamHistory pth = pgh.TeamHistories[0];
 
-            Portfolio = pth.Items.LastOrDefault();
+                Portfolio = pth.Items.LastOrDefault();
+            }   
         }
     }
 }
