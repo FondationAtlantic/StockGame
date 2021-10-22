5288ee29c42978437b97c071f23d5fa2df52d59e
Andronik Karkaselis <akarkaselis@nmedia.ca>
2021-10-22 10:18:35 -0400

    Gains and losses

--- StockGame/Pages/Index.cshtml.cs
+++ StockGame/Pages/Index.cshtml.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using StockGame.Data;
 using StockGame.Models;
 using StockGame.Models.ViewModels;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace StockGame.Pages
 {
@@ -17,19 +16,35 @@ namespace StockGame.Pages
         {
         }
 
+        [Display(Name = "Valeur"), DataType(DataType.Currency)]
+        public float GainsLosses { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.00} %")]
+        public float GainsLossesPercentage { get; set; }
         public bool HasJoinedTeam { get; set; }
+        public AnalysisIndexItem HighestYield { get; set; }
+        public AnalysisIndexItem LowestYield { get; set; }
         public IList<AnalysisIndexItem> IndexItems { get; set; }
         public PortfolioHistoryItem Portfolio { get; set; }
+
+        public async Task FetchPortfolio()
+        {
+            await FindActiveGameAndTeam();
+
+            PortfolioGameHistory pgh = await PortfolioHistories(ActiveGame, Enumerable.Repeat(ActiveTeam, 1), ActiveEpisodeIndex);
+
+            PortfolioTeamHistory pth = pgh.TeamHistories[0];
+
+            Portfolio = pth.Items.LastOrDefault();
+        }
 
         public async Task OnGetAsync()
         {
             await FindActiveGameAndTeam();
-            //HasJoinedTeam = Context.TeamMembers.Where(tm => tm.UserId == CurrentUser.Id).FirstOrDefault() != null;
 
             if (User.Identity.IsAuthenticated && User.IsInRole("Student"))
             {
                 HasJoinedTeam = Context.TeamMembers.Where(tm => tm.UserId == CurrentUser.Id).FirstOrDefault() != null;
-                
+
                 if (HasJoinedTeam)
                 {
                     await FetchPortfolio();
@@ -53,33 +68,26 @@ namespace StockGame.Pages
                             items.Add(new AnalysisIndexItem
                             {
                                 EpisodeEquityInfo = eei,
-                                Trend = (iterPastEquityInfos.Current == null || iterPastEquityInfos.Current.EquityId != eei.EquityId || iterPastEquityInfos.Current.Price == eei.Price)
+                                Trend = (iterPastEquityInfos.Current == null
+                                        || iterPastEquityInfos.Current.EquityId != eei.EquityId
+                                        || iterPastEquityInfos.Current.Price == eei.Price)
                                         ? AnalysisIndexItem.PriceTrend.Unchanged
-                                        : (iterPastEquityInfos.Current.Price < eei.Price ? AnalysisIndexItem.PriceTrend.Up : AnalysisIndexItem.PriceTrend.Down)
+                                        : (iterPastEquityInfos.Current.Price < eei.Price
+                                        ? AnalysisIndexItem.PriceTrend.Up
+                                        : AnalysisIndexItem.PriceTrend.Down)
                             });
                         }
-
                         iterPastEquityInfos.MoveNext();
                         iterPastEquityInfos.MoveNext();
                     }
                     IndexItems = items.OrderBy(item => item.EpisodeEquityInfo.Equity.Industry.Name).ThenBy(item => item.EpisodeEquityInfo.Equity.Name).ToList();
+                    HighestYield = items.OrderByDescending(item => item.DividendYield.HasValue).ThenByDescending(items => items.DividendYield).First();
+                    LowestYield = items.OrderByDescending(item => item.DividendYield.HasValue).ThenBy(item => item.DividendYield).First();
+
+                    GainsLosses = Portfolio.TotalValue - ActiveGame.InitialCash;
+                    GainsLossesPercentage = ((GainsLosses / ActiveGame.InitialCash) * 100);
                 }
-                
             }
-        }
-
-        public async Task FetchPortfolio()
-        {
-            await FindActiveGameAndTeam();
-
-            if (User.Identity.IsAuthenticated && User.IsInRole("Student"))
-            {
-                PortfolioGameHistory pgh = await PortfolioHistories(ActiveGame, Enumerable.Repeat(ActiveTeam, 1), ActiveEpisodeIndex);
-
-                PortfolioTeamHistory pth = pgh.TeamHistories[0];
-
-                Portfolio = pth.Items.LastOrDefault();
-            }   
         }
     }
 }
